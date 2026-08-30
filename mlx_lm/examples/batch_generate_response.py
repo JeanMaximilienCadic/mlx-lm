e8ccac18baf49a3e6b0949cc5ffe737979769aa5
Cheng <git@zcbenz.com>
2026-08-31 08:27:39 +0900

    wip

--- mlx_lm/examples/batch_generate_response.py
+++ mlx_lm/examples/batch_generate_response.py
@@ -3,7 +3,7 @@
 from mlx_lm import batch_generate, load
 
 # Specify the checkpoint
-checkpoint = "mlx-community/Llama-3.2-3B-Instruct-4bit"
+checkpoint = "mlx-community/Qwen3.8-27B-4bit"
 
 # Load the corresponding model and tokenizer
 model, tokenizer = load(path_or_hf_repo=checkpoint)
@@ -21,6 +21,7 @@ prompts = [
     tokenizer.apply_chat_template(
         [{"role": "user", "content": p}],
         add_generation_prompt=True,
+        enable_thinking=False,
     )
     for p in prompts
 ]
@@ -29,23 +30,4 @@ prompts = [
 result = batch_generate(
     model, tokenizer, prompts, verbose=False, return_prompt_caches=True, max_tokens=2048
 )
-print(result.texts[-1])
-
-prompts = [
-    "Could you summarize that?",
-    "And what about the sea?",
-    "Try again?",
-    "And Mt Olympus?",
-]
-prompts = [
-    tokenizer.apply_chat_template(
-        [{"role": "user", "content": p}],
-        add_generation_prompt=True,
-    )
-    for p in prompts
-]
-
-result = batch_generate(
-    model, tokenizer, prompts, verbose=False, prompt_caches=result.caches
-)
-print(result.texts[-1])
+print(result.texts)
